54f8efc09170d268a1f5dd58ccdd7624d46c4aa9
deloading4-maker <deloading4@gmail.com>
2026-03-31 19:37:22 +0300

    fix json

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json()); // ОБЯЗАТЕЛЬНО
+// middlewares
+app.use(cors());
+app.use(express.json());
 
+// 🔥 ПОДКЛЮЧЕНИЕ К MONGODB (ТВОЁ)
+mongoose.connect("mongodb+srv://howlor:howlor123@cluster0.yelzxdr.mongodb.net/shop?retryWrites=true&w=majority")
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+// тестовый роут
 app.get("/", (req, res) => {
-  res.send("API работает");
+  res.send("API WORKING");
 });
 
+// логин
 app.post("/login", (req, res) => {
   const { login, password } = req.body;
 
@@ -19,10 +29,17 @@ app.post("/login", (req, res) => {
   }
 });
 
+// порт
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
 
-mongodb+srv://howlor:howlor123@cluster0.yelzxdr.mongodb.net/shop?retryWrites=true&w=majority
+app.get("/login", (req, res) => {
+  res.send("LOGIN ENDPOINT WORKS");
+});
+
+app.get("/test", (req, res) => {
+  res.send("OK");
+});
