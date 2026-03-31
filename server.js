3255bd05877b8f69fc252f733a67377a7fb32ecc
deloading4-maker <deloading4@gmail.com>
2026-03-31 19:56:52 +0300

    fix mongo

--- server.js
+++ server.js
@@ -1,34 +1,9 @@
-console.log("SERVER STARTED");
-app.get("/", (req, res) => {
-  res.send("WORKS");
-});
-const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
-app.use(express.json());
-
-// тест
-app.get("/", (req, res) => {
-  res.send("API WORKING");
-});
-
-// логин
-app.post("/login", (req, res) => {
-  const { login, password } = req.body;
-
-  if (login === "admin" && password === "123") {
-    res.json({ success: true });
-  } else {
-    res.status(401).json({ success: false });
-  }
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
-
-app.get("*", (req, res) => {
-  res.send("ROUTE NOT FOUND");
-});
+mongoose.connect("mongodb+srv://howlor:howlor123@cluster0.yelzxdr.mongodb.net/shop")
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB error:", err);
+  });
