2fb1243a915907cfc169d49f7271f4cf3c2027f4
deloading4-maker <deloading4@gmail.com>
2026-03-31 20:18:19 +0300

    fix backend path

--- server.js
+++ server.js
@@ -21,8 +21,9 @@ app.post("/login", (req, res) => {
 });
 
 // 🚀 START SERVER
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 mongoose.connect("mongodb+srv://howlor:howlor123@cluster0.yelzxdr.mongodb.net/shop?retryWrites=true&w=majority")
