a4abaa6e594b87ea2a47a8c33e408bdf9913ee5d
deloading4-maker <deloading4@gmail.com>
2026-03-31 19:59:30 +0300

    add login route

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
+const express = require("express");
 const mongoose = require("mongoose");
 
+const app = express();
+app.use(express.json());
+
+// 🔗 MongoDB
 mongoose.connect("mongodb+srv://howlor:howlor123@cluster0.yelzxdr.mongodb.net/shop")
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.error("MongoDB error:", err);
-  });
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+// 🔐 LOGIN
+app.post("/login", (req, res) => {
+  const { login, password } = req.body;
+
+  if (login === "admin" && password === "123") {
+    res.json({ success: true });
+  } else {
+    res.status(401).json({ success: false });
+  }
+});
+
+// 🚀 START SERVER
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
