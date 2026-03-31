23beb25d2bb8169038885bc85fcfccabcd04d8a6
deloading4-maker <deloading4@gmail.com>
2026-03-31 18:47:51 +0300

    deploy fix

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json()); // ОБЯЗАТЕЛЬНО
+
+app.get("/", (req, res) => {
+  res.send("API работает");
+});
+
+app.post("/login", (req, res) => {
+  const { login, password } = req.body;
+
+  if (login === "admin" && password === "123") {
+    res.json({ success: true });
+  } else {
+    res.status(401).json({ success: false });
+  }
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
